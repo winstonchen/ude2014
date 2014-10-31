e0759cda936b8a00ffba9ab3936619e4915ccf41
Winston Chen <winstonc@bu.edu>
2014-10-30 20:42:47 -0400

    conse logs the item names and nothing else

--- js/item.js
+++ js/item.js
@@ -3,7 +3,6 @@ $.support.cors = true;
 var devKey = "4npra7dguufgq5575kkbdj9p";
 $("#submitSearch").click(function(){
 	var item = document.getElementById("itemSearch").value;
-	var returnData;
 	$.ajax({
 	  	url: "http://api.remix.bestbuy.com/v1/products(search="+item+")",
 	  	dataType:"jsonp",
@@ -14,14 +13,8 @@ $("#submitSearch").click(function(){
 	  		apiKey: '4npra7dguufgq5575kkbdj9p'
 	  	},
 	  	success: function(response){
-	  		$.getJSON(response, function(json))
-	  		var count = Object.keys(returnData.parse).length;
-			var data = [];
-			for(var i = 0; i < count; i++){
-     			 data.push(returnData.parse[i]);
-    		}
-	  		var products = _.pluck(data, "products");
-	  		console.log(products);
-	  	}
-	  });
+	  		var data = response;
+	  		console.log(data.products);
+	    }
+	});
 });
