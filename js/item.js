5b94a663019a6cab4982159d40fa1afdd7b613b6
Winston Chen <winstonc@bu.edu>
2014-10-30 20:08:08 -0400

    fixed cors

--- js/item.js
+++ js/item.js
@@ -5,7 +5,7 @@ $("#submitSearch").click(function(){
 	var item = document.getElementById("itemSearch").value;
 	var returnData;
 	$.ajax({
-	  	url: "http://api.remix.bestbuy.com/v1/products(name="+item+")",
+	  	url: "http://api.remix.bestbuy.com/v1/products(search="+item+")",
 	  	dataType:"jsonp",
 	  	cache: true,
 	  	data: {
@@ -14,7 +14,14 @@ $("#submitSearch").click(function(){
 	  		apiKey: '4npra7dguufgq5575kkbdj9p'
 	  	},
 	  	success: function(response){
-	  		console.log(response);
+	  		$.getJSON(response, function(json))
+	  		var count = Object.keys(returnData.parse).length;
+			var data = [];
+			for(var i = 0; i < count; i++){
+     			 data.push(returnData.parse[i]);
+    		}
+	  		var products = _.pluck(data, "products");
+	  		console.log(products);
 	  	}
 	  });
 });
