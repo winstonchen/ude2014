c2a7c2aa8500ac77412f24520b2d830f91c6e6d1
Winston Chen <winstonc@bu.edu>
2014-10-30 21:58:50 -0400

    changed to function

--- js/item.js
+++ js/item.js
@@ -1,28 +1,29 @@
 jQuery.support.cors = true;
 $.support.cors = true;
-var itemid = [];
-$("#submitSearch").click(function(){
-	var item = document.getElementById("itemSearch").value;
-	$.ajax({
-	  	url: "http://api.remix.bestbuy.com/v1/products(search="+item+")",
-	  	dataType:"jsonp",
-	  	cache: true,
-	  	data: {
-	  		format: 'json',
-	  		apiKey: '4npra7dguufgq5575kkbdj9p'
-	  	},
-	  	success: function(response){
-	  		$('#displayItems').children('div').remove();
-	  		var data = response;
-	  		console.log(data);
-	  		for(var i = 0; i < 10; i++){
-	  			$("#displayItems").append("<div class = 'item'><p class = 'itemname'> "+data.products[i].name+ "</p><p class = 'itemPrice'>"+data.products[i].salePrice+"<img src = '"+data.products[i].image+"'></div>");
-	  			if(data.products[i].customerReviewAverage == null){
-	  				$("#displayItems").append("<p class = 'rating'> Rating: No Review</p>");
-	  			} else {
-	  				$("#displayItems").append("<p class = 'rating'> Rating: "+data.products[i].customerReviewAverage+"</p>");
-	  			}
-	  		}
-	  	}
+function search(){
+	$("#submitSearch").click(function(){
+		var item = document.getElementById("itemSearch").value;
+		$.ajax({
+		  	url: "http://api.remix.bestbuy.com/v1/products(search="+item+")",
+		  	dataType:"jsonp",
+		  	cache: true,
+		  	data: {
+		  		format: 'json',
+		  		apiKey: '4npra7dguufgq5575kkbdj9p'
+		  	},
+		  	success: function(response){
+		  		$('#displayItems').children('div').remove();
+		  		var data = response;
+		  		console.log(data);
+		  		for(var i = 0; i < 10; i++){
+		  			$("#displayItems").append("<div class = 'item'><p class = 'itemname'> "+data.products[i].name+ "</p><p class = 'itemPrice'>"+data.products[i].salePrice+"<img src = '"+data.products[i].image+"'></div>");
+		  			if(data.products[i].customerReviewAverage == null){
+		  				$("#displayItems").append("<p class = 'rating'> Rating: No Review</p>");
+		  			} else {
+		  				$("#displayItems").append("<p class = 'rating'> Rating: "+data.products[i].customerReviewAverage+"</p>");
+		  			}
+		  		}
+		  	}
+		});
 	});
-});
+}
