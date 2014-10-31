a0093ba737e5dfb179a29a5f4046fcc3fdf19bae
Winston Chen <winstonc@bu.edu>
2014-10-30 21:46:15 -0400

    added reviews

--- js/item.js
+++ js/item.js
@@ -14,8 +14,14 @@ $("#submitSearch").click(function(){
 	  	success: function(response){
 	  		$('#displayItems').children('div').remove();
 	  		var data = response;
+	  		console.log(data);
 	  		for(var i = 0; i < 10; i++){
 	  			$("#displayItems").append("<div class = 'item'><p class = 'itemname'> "+data.products[i].name+ "</p><p class = 'itemPrice'>"+data.products[i].salePrice+"<img src = '"+data.products[i].image+"'></div>");
+	  			if(data.products[i].customerReviewAverage == null){
+	  				$("#displayItems").append("<p class = 'rating'> Rating: No Review</p>");
+	  			} else {
+	  				$("#displayItems").append("<p class = 'rating'> Rating: "+data.products[i].customerReviewAverage+"</p>");
+	  			}
 	  		}
 	  	}
 	});
